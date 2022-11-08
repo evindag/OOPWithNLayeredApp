479a0f569179a0743a693ddf392ab547d5f38dea
evindag <80323753+evindag@users.noreply.github.com>
2022-11-09 02:08:17 +0300

    Oop App

---
+++ oopWithNLayeredApp/src/oopWithNLayeredApp/dataAccess/HibernateProductDao.java
@@ -0,0 +1,9 @@
+package oopWithNLayeredApp.dataAccess;
+
+import oopWithNLayeredApp.entities.Product;
+
+public class HibernateProductDao {
+	public void add(Product product) {
+		System.out.println("Hibernate ile veritabanına eklendi");
+	}
+}
