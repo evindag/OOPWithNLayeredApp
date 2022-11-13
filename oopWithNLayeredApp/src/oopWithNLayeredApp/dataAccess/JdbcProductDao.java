344d8cadc43f6b70d575e7991773a53280dd0f58
evindag <80323753+evindag@users.noreply.github.com>
2022-11-13 16:36:22 +0300

    JdbsProduct

--- oopWithNLayeredApp/src/oopWithNLayeredApp/dataAccess/JdbcProductDao.java
+++ oopWithNLayeredApp/src/oopWithNLayeredApp/dataAccess/JdbcProductDao.java
@@ -2,7 +2,7 @@ package oopWithNLayeredApp.dataAccess;
 
 import oopWithNLayeredApp.entities.Product;
 
-public class JdbcProductDao {
+public class JdbcProductDao implements ProductDao{
 	public void add(Product product) {
 		System.out.println("JDBC ile veritabanına eklendi.");
 	}
